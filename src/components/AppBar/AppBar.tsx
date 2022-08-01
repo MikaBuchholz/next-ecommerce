cb7013739793d56dd5b71c2652b97197643ef228
MikaBuchholz <mika.buchholz3@gmail.com>
2022-08-01 18:10:43 +0200

    Not final Appbr comp

---
+++ src/components/AppBar/AppBar.tsx
@@ -0,0 +1,21 @@
+import { NextPage } from "next";
+import styles from "./AppBar.module.css";
+import Link from "next/link";
+
+const AppBar: NextPage = () => {
+  return (
+    <div className={styles.appContainer}>
+      <h1 className={styles.title}>Bike Factory</h1>
+      <div className={styles.navigation}>
+        <Link href="/about">
+          <a className={styles.link}>About</a>
+        </Link>
+        <Link href="/contact">
+          <a className={styles.link}>Contact</a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default AppBar;
