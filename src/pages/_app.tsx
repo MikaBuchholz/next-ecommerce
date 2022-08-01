aeed836d237c2196144be230d3019b908a18597b
MikaBuchholz <mika.buchholz3@gmail.com>
2022-08-01 18:11:37 +0200

    Minor changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "~/styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
+import AppBar from "~/components/AppBar/AppBar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <AppBar />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Index 🤩</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 };
 
 export default Home;
