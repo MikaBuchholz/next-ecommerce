baa43a9a83a3d24f66275f73d6564268ece1db78
MikaBuchholz <mika.buchholz3@gmail.com>
2022-08-03 20:45:38 +0200

    80% Mobile Support | Full layout rework

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,59 +9,57 @@ const Home: NextPage = () => {
 
   return (
     <div>
-      <div className={styles.leftContainer}>
-        <div className={styles.center}>
-          <h1 className={styles.buildHeadline}>
-            Create <span className={styles.your}>Your</span> Bike
-          </h1>
-        </div>
-        <div className={styles.center}>
-          <p>
-            Bike <span className={styles.applyOrange}>Style</span>
-          </p>
-        </div>
+      <div className={styles.topContainer}>
         <BikeSelector />
+        <div className={styles.rightContainer}>
+          <div className={styles.headline}>
+            <h1>
+              Over <span className={styles.applyYellow}>20 times</span> in
+              Berlin. <span className={styles.applyRed}>Loved</span> and
+              respected by{" "}
+              <span className={styles.applyGreen}>
+                customers and competition.
+              </span>{" "}
+              Getting <span className={styles.applyPurple}>your</span> bike was
+              never, this easy. Come{" "}
+              <span className={styles.applyRed}>visit</span> one of our shops in
+              your area.
+            </h1>
+          </div>
+          <div className={styles.buttonContainer}>
+            <button className={styles.searchButton}>Find Shop</button>
+            <button
+              className={`${styles.searchButton} ${styles.applyEmpty}`}
+              onClick={() => {
+                billingRef?.current!.scrollIntoView({ behavior: "smooth" });
+              }}
+            >
+              Billing Plans
+            </button>
+          </div>
+        </div>
       </div>
-      <div className={styles.rightContainer}>
-        <div className={styles.headline}>
+      <div className={styles.offerContainer}>
+        <h1>
+          What We <span className={styles.applyRed}>Offer</span>
+        </h1>
+      </div>
+      <div className={styles.bottomContainer}>
+        <div className={styles.rightBottomContainer}>
           <h1>
-            Over <span className={styles.applyYellow}>20 times</span> in Berlin.
-            Loved and respected by{" "}
-            <span className={styles.applyGreen}>
-              customers and competition.
-            </span>{" "}
-            Getting <span className={styles.applyPurple}>your</span> bike was
-            never, this easy. Come{" "}
-            <span className={styles.applyRed}>visit</span> one of our shops in
-            your area.
+            <span className={styles.applyOrange}>You decide the price</span>.
+            <span className={styles.applyRed}>Three</span> packages{" "}
+            <span className={styles.applyGreen}>tailored</span> to your needs.{" "}
+            <span className={styles.applyPurple}>Unbeatable prices</span> and{" "}
+            <span className={styles.applyRed}>excellent execution</span> of
+            work. Looking for something{" "}
+            <span className={styles.applyYellow}>different?</span> Come{" "}
+            <span className={styles.applyPurple}>talk</span> with us.
           </h1>
         </div>
-        <div className={styles.buttonContainer}>
-          <button className={styles.searchButton}>Find Shop</button>
-          <button
-            className={`${styles.searchButton} ${styles.applyEmpty}`}
-            onClick={() => {
-              billingRef?.current!.scrollIntoView({ behavior: "smooth" });
-            }}
-          >
-            Billing Plans
-          </button>
+        <div ref={billingRef} className={styles.billingContainer}>
+          <BillingBox />
         </div>
-      </div>
-
-      <div className={styles.rightBottomContainer}>
-        <div>Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
-        <div>
-          Quia modi architecto aperiam asperiores, explicabo perferendis!
-        </div>
-        <div>Eaque nam, distinctio assumenda laboriosam reiciendis quod.</div>
-        <div>
-          Eaque beatae accusantium cupiditate aspernatur. Suscipit, iste.
-        </div>
-        <div>Possimus qui odio facere adipisci, labore aperiam.</div>
-      </div>
-      <div ref={billingRef} className={styles.billingContainer}>
-        <BillingBox />
       </div>
     </div>
   );
