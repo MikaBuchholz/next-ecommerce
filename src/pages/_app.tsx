86d39b45d125febd6d63244d09a3acc8753497d0
MikaBuchholz <mika.buchholz3@gmail.com>
2022-08-05 19:22:54 +0200

    Added footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "~/styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
 import AppBar from "~/components/AppBar/AppBar";
+import Footer from "~/components/Footer/Footer";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
       <AppBar />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 };
